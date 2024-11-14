210bd13a22934c474aa8f67529558fa45721017d
Manthanchauhan18 <manthanchauhan18320000@gmail.com>
2024-11-14 15:58:57 +0530

    datatypes and ECMA Script

---
+++ JavaScript Learning/03_Datatypes_and_ECMA_standards.js
@@ -0,0 +1,33 @@
+"use strict"        //treat all code as newer version of JS
+
+// alert(3+3)      // it will gives an error :- alert is not defined because we are writing code in vscode and it is a functionality of browser 
+
+// console.log(3+3) console.log("Manthan") // it will show an error because both lines are saprate lines but you write it in single line 
+console.log(3+3); console.log("Mantahn")    // it will not give error because we define line end point semicolan(;)
+
+// but dont write code like this it is a bad habbit of developer so try to write code which is readable easily like below
+console.log(3+3)
+console.log("Manthan")
+
+
+//datatypes
+let name = "Manthan"    // string
+let age = 18     // number
+let isLoggedIn = true   // boolean
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone type
+// undefined => value not initialize
+// symbol => used for uniqueness
+// object 
+
+
+console.log(typeof "Manthan")   // return string
+console.log(typeof age)         // return number
+console.log(typeof null)        // return object
+console.log(typeof undefined)   // return undefined
+
